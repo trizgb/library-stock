8887d92543fc604e25ebce7fcb2f9498c76841f7
Beatriz Gomez <beatrizgomez92@gmail.com>
2019-04-07 19:49:58 +0200

    Run build docs, Github Pages

---
+++ docs/precache-manifest.ea26759beb76ac3b5488a2b905910daa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d85c31983073b676045d",
+    "url": "./static/js/main.5d2e8b75.chunk.js"
+  },
+  {
+    "revision": "7dcc1256c3d42b5b5a28",
+    "url": "./static/js/2.7901747f.chunk.js"
+  },
+  {
+    "revision": "d85c31983073b676045d",
+    "url": "./static/css/main.372d0248.chunk.css"
+  },
+  {
+    "revision": "300a5f3431e6ff5e8df8c8b0e72e1441",
+    "url": "./index.html"
+  }
+];
